7f9228596d7bd4453abc1d1680a1e6205dc99a7e
8smvvj4wzy-sketch <8smvvj4wzy@privaterelay.appleid.com>
2026-07-29 21:28:02 +0200

    Delete aba-cadre directory

--- aba-cadre/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Nom du dépôt GitHub : à adapter si le dépôt porte un autre nom que celui-ci
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-});
